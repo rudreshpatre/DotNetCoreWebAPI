79cb674abdf2cb283367a55692fb1b8e5bd3bdab
Rudresh Patre <rudreshpatre@gmail.com>
2020-10-27 21:20:51 +0530

    Added RootController with GetRoot endpoint

---
+++ LandonApi/Controllers/RootController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot() 
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null)
+            };
+
+            return Ok(response);
+        }
+    }
+}
--- LandonApi/Startup.cs
+++ LandonApi/Startup.cs
@@ -26,6 +26,7 @@ namespace LandonApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddRouting(options => options.LowercaseUrls = true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
